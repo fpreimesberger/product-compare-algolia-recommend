08872a0d37ab06cfab26b4629cda8f1af950f5d6
Freya Preimesberger <freyapreimesberger@REM-MBP-FP.fios-router.home>
2023-03-31 15:18:06 -0400

    fixed keys

--- src/components/Table.js
+++ src/components/Table.js
@@ -9,10 +9,6 @@ const formatString = (str) => {
   return output.replace("\\", '"');
 };
 
-<IconContext.Provider
-  value={{ color: "yellow", className: "yellow-icon" }}
-></IconContext.Provider>;
-
 const CompareTable = (props) => {
   return (
     <div className="compare-table">
@@ -82,31 +78,29 @@ const CompareTable = (props) => {
           <tr>
             <th scope="row">Rating</th>
             {props.recommendations.map((recommendation) => (
-              <>
-                <td scope="col" key={recommendation.objectID}>
-                  <span className="hit-em hit-rating">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="8"
-                      height="8"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fill="#e2a400"
-                        fillRule="evenodd"
-                        d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"
-                      />
-                    </svg>{" "}
-                    {recommendation.rating}
-                  </span>
-                </td>
-              </>
+              <td scope="col" key={recommendation.objectID}>
+                <span className="hit-em hit-rating">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="8"
+                    height="8"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill="#e2a400"
+                      fillRule="evenodd"
+                      d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"
+                    />
+                  </svg>{" "}
+                  {recommendation.rating}
+                </span>
+              </td>
             ))}
           </tr>
           <tr>
             <th scope="row">Free Shipping</th>
             {props.recommendations.map((recommendation) => (
-              <td scope="col" class="shipping" key={recommendation.objectID}>
+              <td scope="col" key={recommendation.objectID}>
                 {" "}
                 {recommendation.free_shipping ? (
                   <SlCheck color="#e2a400" fontSize="1.25em" />
